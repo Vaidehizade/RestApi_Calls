ded64ddfa0aabdc5ed5d96a12094083b8fb42076
Vaidehizade <vaidehizade2001@gmail.com>
2021-10-14 19:57:51 +0530

    fetched data

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import List from './component/List';
+import withListLoading from './component/withListLoading';
+function App() {
+  const ListLoading = withListLoading(List); // assigning higher order component to a new component
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/hacktivist123/repos`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '} with 
+          <span role='img' aria-label='love'>
+            💚
+          </span>{' '}
+          by Vaidehi 
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default App;
+
